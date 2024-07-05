3639e08e954eaba5b0c1d6c8f85016aeabd5c361
Kendal Percimoney <48430773+kendalpercimoney@users.noreply.github.com>
2024-07-05 15:28:10 +0100

    Initial commit

---
+++ TwoSumOn^2.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+var twoSum = function(nums, target) {
+
+for (let i = 0; i < nums.length; i++){
+
+    let firstNum = nums[i];
+
+    for (let j = 0; j < nums.length; j++) {
+
+        if (i != j){
+            let secondNum = nums[j];
+            let sum = firstNum + secondNum;
+
+            if (sum === target){
+
+            const answer = [i, j]
+            return (answer);
+            
+        }
+        
+    }
+
+    }
+
+}
+
+
+};
