2e91cff3200d2926bf2d54a9fd58eb50d55bb805
Kendal Percimoney <48430773+kendalpercimoney@users.noreply.github.com>
2024-08-03 00:01:31 +0100

    Update AddTwoNumbers.js
    Figured it out!

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -4,13 +4,27 @@ let l2 = [5,6,4];
 
 
     let numberOne = 0;
+    let numberTwo = 0;
     
     for (let i = 0; i < l1.length; i++) {
-        numberOne = 10^ i (numberOne + l1[i]);
+        if (i === 0){
+            numberOne = (l1[i]);
+        } else {
+            numberOne += ((Math.pow(10, i)) * (l1[i]));
+        }
+    }
 
+    for (let i = 0; i < l2.length; i++) {
+        if (i === 0){
+            numberTwo = (l2[i]);
+        } else {
+            numberTwo += ((Math.pow(10, i)) * (l2[i]));
+        }
     }
 
     console.log(numberOne);
+    console.log(numberTwo);
+    console.log(numberOne + numberTwo);
    
     
 
