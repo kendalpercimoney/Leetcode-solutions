7163ddf892602a20ddeda74137e96b9f361990bb
Kendal Percimoney <48430773+kendalpercimoney@users.noreply.github.com>
2024-08-02 23:44:50 +0100

    Update AddTwoNumbers.js

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -1,22 +1,16 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function(l1, l2) {
 
-    let numberOne;
+let l1 = [2,4,3];
+let l2 = [5,6,4];
+
+
+    let numberOne = 0;
     
-        for (let i = 0; i < l1.length; i++) {
-            numberOne = numberOne + l1[i];
-        }
+    for (let i = 0; i < l1.length; i++) {
+        numberOne = 10^ i (numberOne + l1[i]);
+
+    }
+
+    console.log(numberOne);
+   
     
-        return (numberOne);
-    };
+
